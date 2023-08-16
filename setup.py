7e31dec60ca0cfb6d0622c7794499cba153a279f
David Currie <dcurrie27@gmail.com>
2023-08-16 17:15:48 +0100

    init

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='what are the haps with cookie cutter',
+    author='dc27',
+    license='MIT',
+)
